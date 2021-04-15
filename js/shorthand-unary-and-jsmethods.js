9e945d0dcdbe62ddfb13b15c7b55e357f14b8b5d
Richard Lara <richardlara@Richards-Air.attlocal.net>
2021-04-15 16:11:00 -0500

    JS Exercise

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// SHORTHAND ASSIGNMENT OPERATORS
+
+// += / -=
+
+// var name = "Richard";
+
+// name = name + "" +"Lara"
+// console.log(name);
+
+// name += " Lara";
+// console.log(name);
+
+// console.log(name-= "Lara");
+
+// var daysRemainingInCohort = 114;
+//
+// daysRemainingInCohort -= 4;
+//
+// console.log(daysRemainingInCohort);
+
+// MINI EXERCISE
+// TODO: REFRACTOR THE MINI EXERCISE ON ARITHMETIC OPERATOR TO USE VARIABLES AND THEIR SHORTHAND OPERATORS
+
+var eq1 = 2;
+console.log(eq1 += 2);
+
+var eq2 = 360;
+console.log(eq2 -= 356);
+
+var eq3 = 2;
+console.log(eq3 *= 2);
+
+var eq4 = 20;
+console.log(eq4 /= 5);
+
+var eq5 = 28;
+console.log(eq5 %= 6);
