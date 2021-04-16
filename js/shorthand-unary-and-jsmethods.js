42bd92a47990ff2e26c0952551e75b25cc165078
Richard Lara <richardlara@Richards-Air.attlocal.net>
2021-04-16 09:29:07 -0500

    JS Exercises

--- js/shorthand-unary-and-jsmethods.js
+++ js/shorthand-unary-and-jsmethods.js
@@ -23,17 +23,68 @@
 // MINI EXERCISE
 // TODO: REFRACTOR THE MINI EXERCISE ON ARITHMETIC OPERATOR TO USE VARIABLES AND THEIR SHORTHAND OPERATORS
 
-var eq1 = 2;
-console.log(eq1 += 2);
+// var eq1 = 2;
+// console.log(eq1 += 2);
+//
+// var eq2 = 360;
+// console.log(eq2 -= 356);
+//
+// var eq3 = 2;
+// console.log(eq3 *= 2);
+//
+// var eq4 = 20;
+// console.log(eq4 /= 5);
+//
+// var eq5 = 28;
+// console.log(eq5 %= 6);
+//
 
-var eq2 = 360;
-console.log(eq2 -= 356);
+// UNARY
 
-var eq3 = 2;
-console.log(eq3 *= 2);
+// var makeItANumber = "doge";
+//
+// console.log(typeof(+makeItANumber));
 
-var eq4 = 20;
-console.log(eq4 /= 5);
+// INCREMENT/DECREMENT
 
-var eq5 = 28;
-console.log(eq5 %= 6);
+// post-increment (num++)
+
+// var num = 10;
+//
+// console.log(num--);
+// console.log(num);
+
+// JS BUILT-IN METHODS
+
+// toString();
+
+// var num = 5 * 5;
+// num = num.toString();
+//
+// console.log(num)
+// console.log(typeof num)
+
+// var pi = 3.1415926535;
+//
+// console.log(pi.toFixed(0));
+
+// NUMBER()
+
+// console.log(Number("Richard"));
+
+// isNaN() - checks to see if the value provides is Not A Number(NaN)
+
+// console.log(isNaN(-0));
+// console.log(isNaN("Hi"));
+
+//parseInt()
+
+// console.log(parseInt("12548118 what a number!"));
+// console.log(parseInt("YO 12548118 what a number!"));
+// console.log(parseInt("12548118 4151"));
+
+// Length
+
+console.log("Casey".length);
+console.log("Casey".indexOf("a"));
+    //Not a count of all "a's" in a string, console shows first location of "a"
---
+++ js_exercises/final_intro_exercises.js
@@ -0,0 +1,177 @@
+"use strict";
+
+// var a = 1;
+//     console.log(a);
+// var b = a++;
+//     console.log(b);
+// var c = ++a;
+//     console.log(c);
+
+// what is the value of a, b, and c?
+//
+// var d = "hello";
+//     console.log(d);
+//
+// var e = false;
+//     console.log(e);
+//
+// d++;
+//     console.log(d);
+//
+// e++;
+//     console.log(e);
+
+
+// var perplexed;
+//     console.log(perplexed);
+// perplexed is undefined (no value is assigned)
+// perplexed + 2;
+//     console.log(perplexed);
+//
+// var price = 2.7;
+// price.toFixed(2);
+//     console.log(price);
+
+// var stringPrice = "2.7";
+// stringPrice.toFixed(2);
+
+// var q1 = isNaN(0);
+//     console.log(q1);
+//
+// var q2 = isNaN(1);
+//     console.log(q2);
+//
+// var q3 = isNaN("");
+//     console.log(q3);
+//
+// var q4 = isNaN("string");
+//     console.log(q4);
+//
+// var q5 = isNaN("0");
+//     console.log(q5);
+//
+// var q6 = isNaN("1");
+//     console.log(q6);
+//
+// var q7 = isNaN("3.145");
+//     console.log(q7);
+//
+// var q8 = isNaN(Number.MAX_VALUE);
+//     console.log(q8);
+//
+// var q9 = isNaN(Infinity);
+//     console.log(q9);
+//
+// var q10 = isNaN("true");
+//     console.log(q10);
+//
+// var q11 = isNaN(true);
+//     console.log(q11);
+//
+// var q12 = isNaN("false");
+//     console.log(q12);
+//
+// var q13 = isNaN(false);
+//     console.log(q13);
+//
+// console.log(NaN == NaN)
+// to illustrate why the isNaN() function is needed:
+
+
+// console.log(!true);
+//
+// console.log(!false);
+//
+// console.log(!!true);
+//
+// console.log(!!false);
+//
+// console.log(!!0);
+//
+// console.log(!!-0);
+//
+// console.log(!!1);
+//
+// console.log(!!-1);
+//
+// console.log(!!0.1);
+//
+// console.log(!!"hello");
+//
+// console.log(!!"");
+//
+// console.log(!!'');
+//
+// console.log(!!"false");
+//
+// console.log(!!"0");
+
+//-------Exercise 2
+
+// var sample = "Hello Codeup";
+//
+// console.log(sample.length);
+//
+// console.log(sample.toUpperCase());
+//
+// console.log(sample += " Students");
+//
+// console.log(sample.replace("Students", "Class"));
+//
+// console.log(sample.indexOf('c'));
+// console.log(sample.indexOf('C'));
+//
+// console.log(sample.indexOf("p"));
+//
+// console.log(sample.substring(6,12));
+
+//---- Exercise 3
+
+// var price = 3
+//
+// var mermaid = 3
+//
+// var hercules = 1
+//
+// var bear = 5
+//
+// var pay = (mermaid + hercules + bear) * price;
+// console.log("I will be paying $" + pay);
+
+//--------------
+
+// var google = 400
+//
+// var amazon = 380
+//
+// var facebook = 350
+//
+// var paid = (google * 6) + (facebook * 10) + (amazon * 4);
+// console.log(paid);
+
+//--------
+
+// Can enroll in class if and only if = class is not full && new class does not conflict with other classes
+
+// Product offer can be offered only if = (Person is buying more than 2 products && offer has not expired) ||
+// Person is Premimum member && Offer has not expired
+// var buying = 1;
+// var offer = true;
+// var premium = true;
+//
+// var eligible = ((buying > 2 && offer) || (premium && offer));
+// console.log(eligible);
+
+
+//------Exercise 4
+
+// var username = 'codeup';
+// var password = 'notastrongpassword';
+//
+// var tooShort = password >= 5;
+//
+// var duplicate =
+//
+// var tooLong = username <= 20;
+//
+// var space = " "
