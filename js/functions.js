2cc9d1d7de02be488e97ab71b8be4e01d4e804b4
richardlara35 <richard.u.lara35@gmail.com>
2021-04-16 17:16:46 -0500

    test

--- js/functions.js
+++ js/functions.js
@@ -1,59 +1,133 @@
-"use strict";
 
-// var num = 5;
-//
-// num = num + 5;
-//
-// console.log(num);
+(function(){
 
-// creating an interface for other code to use without knowing what happens inside add()
+    "use strict";
 
+/**
+ * TODO:
+ * Create a function called 'sayHello' that takes a parameter 'name'.
+ * When called, the function should return a message that says hello to the passed in name.
+ *
+ * Example
+ * > sayHello("codeup") // returns "Hello, codeup!"
+ */
 
-// function add(num1, num2, num3){ // <---PARAMETERS HERE
-//     return num1 + num2 + num3; //<-- ALWAYS REMEMBER TO ADD RETURN
-// }
-//
-// console.log("The result of add() is: " + add(51515, 546, -51545 )); // <-- ARGUMENTS HERE
-
-// RETURNS THE DIFFERENCE OF SUBTRACTING NUM2 FROM NUM1
-// function subtract (num1, num2){
-//     return num1 - num2;
-// }
-//
-// console.log("The result of subtract() is: " + subtract(10, 6));
-
-//MINI EXERCISE
-// TODO: CREATE FUNCTIONS WHICH WILL RETURN PRODUCT OF TWO NUMBERS AND DIVIDEND OF TWO NUMBERS RESPECTFULLY
-
-// function product (num1, num2){
-//     return num1 * num2;
-// }
-//
-// console.log("The result of product() is: " + product(6, 5));
-//
-// function divide (num1, num2){
-//    return num1 / num2;
-// }
-
-// console.log("The result of divide() is: " + divide(100, 20));
-
-// function addWithDefaults(num1, num2 = 0){
-//     return num1 + num2;
-// }
-//
-// console.log(addWithDefaults(2, 3));
+function sayHello(name1){
+    return "Hello " + name1;
+}
+sayHello("Richard");
 
 
-{
-    {
-        {
+    /**
+     * TODO:
+     * Call the function 'sayHello' and pass your name as a string literal argument.
+     * Store the result of the function call in a variable named 'helloMessage'.
+     *
+     * console.log 'helloMessage' to check your work
+     */
 
-        }
+    var helloMessage = sayHello("Richard");
+ console.log(helloMessage)
+    /**
+     * TODO:
+     * Store your name as a string in a variable named 'myName', and pass that
+     * variable to the 'sayHello' function. You should see the same output in the
+     * console.
+     */
+    var myName = "Richard";
+console.log(sayHello(myName));
+
+// Don't modify the following line, it generates a random number between 1 and 3
+// and stores it in a variable named random
+var random = Math.floor((Math.random() * 3) + 1);
+
+    /**
+     * TODO:
+     * Create a function called 'isTwo' that takes a number as a parameter.
+     * The function should return a boolean value based on whether or not the passed
+     * number is the number 2.
+     *
+     * Example
+     * > isTwo(1) // returns false
+     * > isTwo(2) // returns true
+     * > isTwo(3) // returns false
+     *
+     * Call the function 'isTwo' passing the variable 'random' as a argument.
+     *
+     * console.log *outside of the function* to check your work (you should see a
+     * different result everytime you refresh the page if you are using the random
+     * number)
+     */
+function isTwo(num){
+    console.log("random number is: " + num)
+    return num === 2;
     }
+    console.log(isTwo(random));
+
+// function isTwo(num){
+      //  console.log("random number is: " + num)
+//     if(num===2){
+//         return true;
+//     } else {
+//         return false;
+//     }
+//         }
+//         console.log(isTwo(random));
+    /**
+     * TODO:
+     * Create a function named 'calculateTip' to calculate a tip on a bill at a
+     * restaurant. The function should accept a tip percentage and the total of the
+     * bill, and return the amount to tip
+     *
+     * Examples:
+     * > calculateTip(0.20, 20) // returns 4
+     * > calculateTip(0.25, 25.50) // returns 6.375
+     * > calculateTip(0.15, 33.42) // returns 5.013
+     */
+function calculateTip(num1, num2){
+    return num1 * num2;
+    }
+    // console.log("You should leave $" + calculateTip(0.20, 20).toFixed(2) + " as tip");
+    // console.log("You should leave $" + calculateTip(0.25, 25.50).toFixed(2) + " as tip");
+    // console.log("You should leave $" + calculateTip(0.15, 33.42).toFixed(2)+ " as tip");
+    /**
+     * TODO:
+     * Use prompt and alert in combination with your calculateTip function to
+     * prompt the user for the bill total and a percentage they would like to tip,
+     * then display the dollar amount they should tip
+     */
+function howMuchWasTheBill(){
+    return prompt("How much was your bill?");
+    }
+function howMuchTip(){
+    return prompt("What percentage would you like to tip?");
 }
 
-function scoping(){
-    var number = 2;
-    console.log(number);
+function totalTip(){
+    var total = howMuchWasTheBill();
+    var tip = howMuchTip();
+
+    alert("You should leave $" + calculateTip(tip, total) + " as a tip");
 }
-console.log(number);
+
+totalTip();
+    /**
+     * TODO:
+     * Create a function named `applyDiscount`. This function should accept a price
+     * (before a discount is applied), and a discount percentage (a number between 0
+     * and 1). It should return the result of applying the discount to the original
+     * price.
+     *
+     * Example:
+     * > var originalPrice = 100;
+     * > var dicountPercent = .2; // 20%
+     * > applyDiscount(originalPrice, dicountPercent) // 80
+     *
+     * > applyDiscount(45.99, 0.12) // 40.4712
+     */
+function applyDiscount(price, discount){
+    return price - (price * discount);
+    }
+console.log("With discount your total is: $" + applyDiscount(80,.20));
+
+})()
---
+++ js/functions_lecture.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// var num = 5;
+//
+// num = num + 5;
+//
+// console.log(num);
+
+// creating an interface for other code to use without knowing what happens inside add()
+
+
+// function add(num1, num2, num3){ // <---PARAMETERS HERE
+//     return num1 + num2 + num3; //<-- ALWAYS REMEMBER TO ADD RETURN
+// }
+//
+// console.log("The result of add() is: " + add(51515, 546, -51545 )); // <-- ARGUMENTS HERE
+
+// RETURNS THE DIFFERENCE OF SUBTRACTING NUM2 FROM NUM1
+// function subtract (num1, num2){
+//     return num1 - num2;
+// }
+//
+// console.log("The result of subtract() is: " + subtract(10, 6));
+
+//MINI EXERCISE
+// TODO: CREATE FUNCTIONS WHICH WILL RETURN PRODUCT OF TWO NUMBERS AND DIVIDEND OF TWO NUMBERS RESPECTFULLY
+
+// function product (num1, num2){
+//     return num1 * num2;
+// }
+//
+// console.log("The result of product() is: " + product(6, 5));
+//
+// function divide (num1, num2){
+//    return num1 / num2;
+// }
+
+// console.log("The result of divide() is: " + divide(100, 20));
+
+// function addWithDefaults(num1, num2 = 0){
+//     return num1 + num2;
+// }
+//
+// console.log(addWithDefaults(2, 3));
+
+
+{
+    {
+        {
+
+        }
+    }
+}
+
+function scoping(){
+    var number = 2;
+    console.log(number);
+}
+console.log(number);
