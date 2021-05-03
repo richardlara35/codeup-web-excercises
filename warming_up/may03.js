813e4472185d9a42bc880c79024bad485cd9c3ed
richardlara35 <richard.u.lara35@gmail.com>
2021-05-03 09:29:53 -0500

    WarmUp

---
+++ warming_up/may03.js
@@ -0,0 +1,12 @@
+(function(){
+"use strict";
+
+function trim(input){
+    // return input.replace(/\s+/g, '');
+    return input.replaceAll(" ","");
+}
+
+    console.log(trim("Not Hello World"));
+
+
+})()
