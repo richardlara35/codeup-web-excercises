6ad8dfe835504aec9db75431d051d41a3bc047f2
richardlara35 <richard.u.lara35@gmail.com>
2021-05-06 16:58:20 -0500

    Daily

--- warming_up/warmups.js
+++ warming_up/warmups.js
@@ -14,26 +14,45 @@
 //------------------------------May 4th
 
 
-    function cube(n){
-        return Math.pow(n,3);
-    }
-
-    console.log(cube(3));
-
-    function cubeRoot(n){
-        return Math.cbrt(n)
-    };
-
-    console.log(cubeRoot(cube(5)));
+    // function cube(n){
+    //     return Math.pow(n,3);
+    // }
+    //
+    // console.log(cube(3));
+    //
+    // function cubeRoot(n){
+    //     return Math.cbrt(n)
+    // };
+    //
+    // console.log(cubeRoot(cube(5)));
 
 //-----------------------------Cinco De Mayo
 
-    function last(input){
-        return (input.slice(-1));
+    // function last(input){
+    //     return (input.slice(-1));
+    // }
+    //
+    // console.log(last("What it do"));
+    // console.log(last("Warm Up!"))
+
+
+//-------------------------------May 6th
+
+    function remainder(num, div) {
+        if (!isNaN(num) && !isNaN(div)) {
+            return num % div;
+        } else if (isNaN(num)) {
+            return (`Parameter ${num} is not a number`);
+        } else if (isNaN(div)) {
+            return (`Parameter ${div} is not a number`);
+        }
+        return ("Its all messed up here");
     }
 
-    console.log(last("What it do"));
-    console.log(last("Warm Up!"))
+    console.log(remainder(26,3));
+    console.log(remainder("Banana",5));
+    console.log(remainder(20,"Jello"));
+    console.log(remainder("Banana","Jello"));
 
 
 
