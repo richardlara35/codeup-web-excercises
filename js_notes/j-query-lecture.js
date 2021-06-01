2a0f228a682308f3d63f453d76e69036675e834e
richardlara35 <richard.u.lara35@gmail.com>
2021-06-01 11:30:26 -0500

    j-query

---
+++ js_notes/j-query-lecture.js
@@ -0,0 +1,34 @@
+(function () {
+    "use strict";
+//
+//     let school = {
+//         students: 36,
+//         name: "Mayde Creek HS",
+//         teachers: ["Casey", "Laura", "Tristan"],
+//         getLunchMoney: function (){
+//             console.log("you have x dollars left");
+//         }
+//     }
+// school.getLunchMoney();
+//
+//     //dollar sign tapping into jQuery library
+//     //selector - the element that you are selecting from the dom
+//     $("selector").methods(any arguments required);
+//
+//
+//     //select by tag name
+//     $("tag-name")
+//     //select by id
+//     $("#id")
+//     //select by class
+//     $(".class")
+//TODO: Using jQuery ...
+// 1. hide the paragraph that is in all caps
+    $("#all-caps").hide();
+// 2.hide all the spans
+    $("span").hide();
+// 3. show all the elements that have a ‘show-me’ class. refresh your page between every task. d
+    $(".show-me").show();
+
+
+})()
