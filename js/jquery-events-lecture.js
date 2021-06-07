63f9929393732490cbce1b04644d6c7cedaa0ca7
richardlara35 <richard.u.lara35@gmail.com>
2021-06-07 13:31:26 -0500

    exercise

--- js/jquery-events-lecture.js
+++ js/jquery-events-lecture.js
@@ -43,31 +43,94 @@
     // function changeImage(card, imgPath){
     //   $(card).find('img').attr('src', imgPath);
     // }
+    //
+    //
+    // $(document).ready(function () {
+    //     $("#submitBtn").click(function () {
+    //         $("#override-bootstrap").css('background-color', 'red')
+    //     })
+    // })
+    //
+    // $(document).ready(function () {
+    //     $(".card-text").click(function () {
+    //         $(this).css('background-color', 'red')
+    //     })
+    // })
+    //
+    // $(document).ready(function () {
+    //     $('#reset').click(function () {
+    //         location.reload();
+    //     })
+    // })
+    // $(document).ready(function () {
+    //     $('#main-title').click(function () {
+    //         $(this).css("text-size","4em");
+    //     })
+    // })
 
 
+    // TODO: Add a data-attribute to each card container (.col-md-3)
+//  -> Give that data-attribute a value of 1-4 (moving top to bottom in the html)
+//  -> Now, when a user hovers over one of the .col-md-3 containers, you should replace the text inside #card-title with the data-attribute value
+//  -> When the user hovers out, the data-attribute value should be replaced with the original text ("Card")
     $(document).ready(function () {
-        $("#submitBtn").click(function () {
-            $("#override-bootstrap").css('background-color', 'red')
-        })
-    })
+        let hoverIn = function () {
+            let attributeValue = $(this).attr("data-attribute");
+            $(this).find(".card-title").text(attributeValue);
+            console.log(attributeValue);
+        }
 
-    $(document).ready(function () {
-        $(".card-text").click(function () {
-            $(this).css('background-color', 'red')
-        })
-    })
+        let hoverOut = function () {
+            $(this).find(".card-title").text("Card");
+        }
 
-    $(document).ready(function () {
-        $('#reset').click(function () {
-            location.reload();
-        })
-    })
-    $(document).ready(function () {
-        $('#main-title').click(function () {
-            $(this).css("text-size","4em");
-        })
-    })
+        $('.col-md-3').hover(hoverIn, hoverOut);
 
+
+// TODO: When an li with the .list-group-item class is clicked, replace the text with the same text, but uppercased
+//  -> ALSO, change the background of the li to a new color
+//  -> When the li is clicked again, reset the li background color to its original color
+//      -> Don't worry about changing the text back
+
+        // $(".list-group-item").toggle(
+        //     function () {
+        //         $(this).css('background-color', 'red');
+        //         $(this).css('text-transform', 'uppercase');
+        //     }, function() {
+        //         $(this).css('background-color', 'white');
+        //     });
+
+
+
+        // $(".list-group-item").click(
+        //     function () {
+        //         $(this).css('background-color', 'red');
+        //         $(this).css('text-transform', 'uppercase');
+        //     }
+        //
+        // )
+
+        $(".list-group-item").click(function(e){
+            $(this).css('text-transform', 'uppercase');
+            $(this).toggleClass('cssStuff');
+        })
+
+
+
+
+        // TODO: Change up the submit button event
+//  -> Add a new input with an id of #redirect-url
+//  -> Remove the old click events from #submitBtn
+//  -> When the user clicks #submitBtn, redirect the page to the value of #redirect-url
+//  -> HINT: You can either add a new input or change the id of an existing input element to #redirect-url to save time
+
+        $('#submitBtn').click(function(){
+        let newSite = $('#redirect-url').val();
+        window.location.assign(newSite);
+
+        })
+
+    })
 
 })();
 
